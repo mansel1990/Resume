fbdd941c8cd8f424f62cd9dd483d4ca5053e26d7
Sanjay Mansel <mansel@Sanjays-MacBook-Air.local>
2023-01-30 21:51:20 +0900

    Tools used animation

--- frontend/src/screens/Skillset.js
+++ frontend/src/screens/Skillset.js
@@ -2,31 +2,6 @@ import React from "react";
 import TechProgressBar from "../components/TechProgressBar";
 
 const Skillset = () => {
-  // const makeNewPosition = () => {
-  //   // Get viewport dimensions (remove the dimension of the div)
-  //   var h = window.innerHeight - 50;
-  //   var w = window.innerWidth - 50;
-
-  //   var nh = Math.floor(Math.random() * h);
-  //   var nw = Math.floor(Math.random() * w);
-
-  //   console.log(nh, nw);
-  //   return [nh, nw];
-  // };
-
-  // const animateDiv = () => {
-  //   const myclass = "tools-used__set-icons";
-  //   var newq = makeNewPosition();
-  //   console.log(newq);
-  //   // myclass.animate({ top: newq[0], left: newq[1] }, 1000, function () {
-  //   //   animateDiv(myclass);
-  //   // });
-  // };
-
-  // useEffect(() => {
-  //   // animateDiv();
-  // });
-
   return (
     <div>
       <div className="skills-title">Tech Stack</div>
@@ -41,80 +16,82 @@ const Skillset = () => {
       <TechProgressBar techName="My SQL" percent="70" />
       <div className="skills-title">Tools Used</div>
       <div className="tools-used">
-        <div className="tools-used__set">
-          <div className="tools-used__set-s1">
-            <div className="tools-used__set-icon-set1">
-              <img
-                src={require("../images/icons/eclipse-icon.jpeg")}
-                alt="Eclipse"
-                className="tools-used__set-icons"
-              />
-              <img
-                src={require("../images/icons/vscode-icon.jpeg")}
-                alt="VS Code"
-                className="tools-used__set-icons"
-              />
-            </div>
-            <div className="tools-used__set-icon-set2">
-              <img
-                src={require("../images/icons/sublime-icon.jpeg")}
-                alt="Sublime"
-                className="tools-used__set-icons"
-              />
-              <img
-                src={require("../images/icons/sql-icon.png")}
-                alt="SQL"
-                className="tools-used__set-icons"
-              />
-            </div>
-            <div className="tools-used__set-icon-set3">
-              <img
-                src={require("../images/icons/toad-icon.png")}
-                alt="Toad"
-                className="tools-used__set-icons"
-              />
-            </div>
-          </div>
-          <div className="tools-used__set-s2">
-            <div className="tools-used__set-icon-set1">
-              <img
-                src={require("../images/icons/git-icon.png")}
-                alt="Git"
-                className="tools-used__set-icons"
-              />
-              <img
-                src={require("../images/icons/jenkins-icon.png")}
-                alt="Jenkins"
-                className="tools-used__set-icons"
-              />
-            </div>
-            <div className="tools-used__set-icon-set2">
-              <img
-                src={require("../images/icons/svn-icon.png")}
-                alt="SVN"
-                className="tools-used__set-icons"
-              />
-              <img
-                src={require("../images/icons/sourcetree-icon.jpeg")}
-                alt="Source Tree"
-                className="tools-used__set-icons"
-              />
-            </div>
-          </div>
-          <div className="tools-used__set-s3">
-            <div className="tools-used__set-icon-set4">
-              <img
-                src={require("../images/icons/jira-icon.jpeg")}
-                alt="JIRA"
-                className="tools-used__set-icons"
-              />
-              <img
-                src={require("../images/icons/rtc-icon.jpeg")}
-                alt="RTC"
-                className="tools-used__set-icons"
-              />
-            </div>
-          </div>
+        <div className="tools-used__icon1">
+          <img
+            src={require("../images/icons/eclipse-icon.jpeg")}
+            alt="Eclipse"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon2">
+          <img
+            src={require("../images/icons/vscode-icon.jpeg")}
+            alt="VS Code"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon3">
+          <img
+            src={require("../images/icons/sublime-icon.jpeg")}
+            alt="Sublime"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon4">
+          <img
+            src={require("../images/icons/sql-icon.png")}
+            alt="SQL"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon5">
+          <img
+            src={require("../images/icons/toad-icon.png")}
+            alt="Toad"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon6">
+          <img
+            src={require("../images/icons/git-icon.png")}
+            alt="Git"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon7">
+          <img
+            src={require("../images/icons/jenkins-icon.png")}
+            alt="Jenkins"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon8">
+          <img
+            src={require("../images/icons/svn-icon.png")}
+            alt="SVN"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon9">
+          <img
+            src={require("../images/icons/sourcetree-icon.jpeg")}
+            alt="Source Tree"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon10">
+          <img
+            src={require("../images/icons/jira-icon.jpeg")}
+            alt="JIRA"
+            className="tools-used__set-icons"
+          />
+        </div>
+        <div className="tools-used__icon11">
+          <img
+            src={require("../images/icons/rtc-icon.jpeg")}
+            alt="RTC"
+            className="tools-used__set-icons"
+          />
         </div>
       </div>
     </div>
